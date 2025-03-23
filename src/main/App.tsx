67d63153c5da1c938a3bdddca1bc34f0065764fa
Stuart Willows <47339755+BearintheWillows@users.noreply.github.com>
2025-03-23 21:39:34 +0000

    refactor: integrate Tailwind CSS and simplify App component

---
+++ src/main/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Header from "./Header.tsx";
+
+function App() {
+
+  return (
+    <>
+      <div>
+          <Header subtitle="TimeTrove" />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/main/Header.tsx
@@ -0,0 +1,14 @@
+﻿
+type Args = {
+    subtitle: string;
+}
+
+const Header = ({subtitle} : Args) => {
+  return (
+    <header className="bg-gray-800 text-white p-4">
+      <h1 className="text-2xl">{subtitle}</h1>
+    </header>
+  );
+}
+
+export default Header;
